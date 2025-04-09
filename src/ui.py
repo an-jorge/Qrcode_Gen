607a9cc0b64f93ae60921482ac48d8581618451e
An Jorge <an.jorge@icloud.com>
2025-04-09 23:03:06 +0100

    Atualização do Label

--- src/ui.py
+++ src/ui.py
@@ -6,6 +6,7 @@ from PIL import ImageTk
 from src.theme import salvar_tema
 from src.qr_code_generator import gerar_qr_code
 
+
 imagem_ref = None
 label_img = None
 entrada_texto = None
@@ -16,29 +17,35 @@ def criar_interface(root, style):
     entrada_texto = tk.StringVar()
 
     def exibir_imagem(imagem):
-        global label_img
+        global label_img, imagem_ref
+
         imagem = imagem.resize((200, 200))
         imagem_ref = ImageTk.PhotoImage(imagem)
 
         if label_img is None:
             label_img = Label(root, image=imagem_ref)
             label_img.image = imagem_ref
-            label_img.place(relx=0.5, rely=0.80, anchor="center")
+            label_img.place(relx=0.5, rely=0.79, anchor="center")
         else:
             label_img.configure(image=imagem_ref)
             label_img.image = imagem_ref
+            label_img.place(relx=0.5, rely=0.79, anchor="center")
 
     def esconder_imagem():
+        global label_img
         if label_img:
-            label_img.place_forget()
+            label_img.place_forget()  # Remove a imagem
+            label_img = None  # Limpa a referência
 
     def gerar_qr(event=None):
         texto = entrada_texto.get()
         if not texto:
-            esconder_imagem()
-            messagebox.showerror("Erro", "Texto não pode estar vázio!")
+            esconder_imagem()  # Remove a imagem caso o campo esteja vazio
+            messagebox.showerror("Erro", "O texto não pode estar vázio!")
             return
+
         imagem = gerar_qr_code(texto)
+        esconder_imagem()  # Esconde a imagem anterior antes de exibir a nova
         exibir_imagem(imagem)
 
     def alternar_tema():
@@ -47,17 +54,18 @@ def criar_interface(root, style):
         style.theme_use(tema_atual)
 
     def ao_digitar(*args):
-        # Verifica se o campo de texto está vazio e esconde a imagem
-        if not entrada_texto.get():
-            esconder_imagem()
+        texto = entrada_texto.get()
+        if not texto:
+            esconder_imagem()  # Esconde a imagem quando o texto for apagado
 
+    # Monitorar o campo de texto para esconder a imagem
     entrada_texto.trace_add("write", ao_digitar)
 
     main_frame = tk.Frame(root, bg=style.colors.bg)
     main_frame.pack(expand=True)
 
     label = Label(main_frame, text="Gerador de Código QR", font=("Arial", 16))
-    input_entry = Entry(main_frame, textvariable=entrada_texto, font=("Arial", 15), width=40)
+    input_entry = Entry(main_frame, textvariable=entrada_texto, font=("Arial", 16), width=40)
     button = Button(main_frame, text="Gerar QR Code", command=gerar_qr, bootstyle=PRIMARY)
     toggle_button = Button(main_frame, text="Alternar Tema", command=alternar_tema, bootstyle=SECONDARY)
 
